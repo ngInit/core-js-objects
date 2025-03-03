987080e7d22f38cce543744c6a59527bb773a470
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-03-03 23:32:04 +0100

    feat: solve makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -124,8 +124,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = new Array(0);
+  Object.keys(lettersObject).forEach((key) => {
+    lettersObject[key].map((value) => {
+      result[value] = key;
+      return value;
+    });
+  });
+  return result.join('');
 }
 
 /**
