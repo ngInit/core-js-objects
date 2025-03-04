8321cbb6137ec17a4e6c15243b241918ba39dabc
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-03-04 20:39:05 +0100

    feat: solve fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -210,8 +210,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newJSON = JSON.parse(json);
+  Object.setPrototypeOf(newJSON, proto);
+  return newJSON;
 }
 
 /**
